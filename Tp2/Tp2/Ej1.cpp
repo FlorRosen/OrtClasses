e8a27360382c14c2326daea7dcfc8e9e87301dc4
Flor Rosental <Flopirosental@hotmail.com>
2018-04-02 00:25:17 -0300

    Tp 2 - manejo de cin

---
+++ Tp2/Tp2/Ej1.cpp
@@ -0,0 +1,22 @@
+//Ingresar dos valores enteros y mostrar el resultado de la suma, de la resta , de la multiplicación  y división de ambos.
+
+#include <iostream>
+using namespace std;
+
+//void main() {
+//	int num1;
+//	int num2;
+//
+//	cout << "Ingresar primer numero" << endl;
+//	cin >> num1;
+//
+//	cout << "Ingresar segundo numero" << endl;
+//	cin >> num2;
+//
+//	cout << "La suma es: " << num1 + num2 << endl;
+//	cout << "La resta es: " << num1 - num2 << endl;
+//	cout << "La multiplicacion es: " << num1 * num2 << endl;
+//	cout << "La division es: " << num1 / num2 << endl;
+//
+//	system("Pause");
+//}
---
+++ Tp2/Tp2/Ej3.cpp
@@ -0,0 +1,25 @@
+//Ingresar 3 notas de una materia. Calculá el promedio.
+
+#include <iostream>
+using namespace std;
+
+//void main() {
+//	int nota1, nota2, nota3;
+//	float promedio;
+//
+//	cout << "Ingresar nota 1" << endl;
+//	cin >> nota1;
+//
+//	cout << "Ingresar nota 2" << endl;
+//	cin >> nota2;
+//
+//	cout << "Ingresar nota 3" << endl;
+//	cin >> nota3;
+//
+//	promedio = (nota1 + nota2 + nota3) / 3;
+//	cout << "El promedio es: " << promedio << endl;
+//
+//	system("Pause");
+//}
+//
+
